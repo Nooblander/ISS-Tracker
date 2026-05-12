67a8e8595eddd2ba7b89a0ea9a3817a421bb4b1e
Thomas N <tomnoorlander@gmail.com>
2026-05-12 17:12:14 +0200

    Decrease timeout for API requests in iss.py
    Reduced timeout values for API requests to improve responsiveness.
    changed longitude and latitude to floats

--- iss.py
+++ iss.py
@@ -4,7 +4,7 @@ import json
 
 def fetch_names():
     try:
-        response = requests.get('http://api.open-notify.org/astros.json' ,timeout=15)
+        response = requests.get('http://api.open-notify.org/astros.json' ,timeout=10)
         response.raise_for_status()
         names = response.json()
         return names
@@ -21,7 +21,7 @@ def fetch_location():
     }
 
     try:
-        response = requests.get('http://api.open-notify.org/iss-now.json', headers=headers, timeout=16 )
+        response = requests.get('http://api.open-notify.org/iss-now.json', headers=headers, timeout=11 )
         response.raise_for_status()
         coords = response.json()
         
@@ -49,8 +49,8 @@ if __name__ == '__main__':
             print ('skipping failled request...')
             continue
         timestamp = coords['timestamp']
-        longitude = coords['iss_position']['longitude']
-        latitude = coords['iss_position']['latitude']
+        longitude = float(coords['iss_position']['longitude'])
+        latitude = float(coords['iss_position']['latitude'])
 
         data = {"timestamp":timestamp, 'coords': {'longitude': longitude, "latitude": latitude}}
         results.append(data)
