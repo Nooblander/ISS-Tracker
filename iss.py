1404adda2905ff81a7c96733153d34a3983f6f44
Thomas N <tomnoorlander@gmail.com>
2026-05-13 11:59:48 +0200

    Add files via upload

--- iss.py
+++ iss.py
@@ -4,7 +4,7 @@ import json
 
 def fetch_names():
     try:
-        response = requests.get('http://api.open-notify.org/astros.json' ,timeout=10)
+        response = requests.get('http://api.open-notify.org/astros.json' ,timeout=9)
         response.raise_for_status()
         names = response.json()
         return names
@@ -36,12 +36,20 @@ def fetch_location():
 if __name__ == '__main__':
     json_names = fetch_names()
     names = []
-    for person in json_names['people']:
-        name = person['name']
-        names.append(name)
+    if json_names is not None:
+        for person in json_names['people']:
+            name = person['name']
+            names.append(name)
+        print ("Names Fetched")
+    else:
+        print ('Fetching names failed, trying again')
+        json_names = fetch_names()
     
     results = []
+    first_timestamp = 0
+    total_time = 0
     i = 0
+    print ()
     while i < 5:
 
         coords = fetch_location()
@@ -51,19 +59,25 @@ if __name__ == '__main__':
         timestamp = coords['timestamp']
         longitude = float(coords['iss_position']['longitude'])
         latitude = float(coords['iss_position']['latitude'])
-
+        if i == 0:
+            first_timestamp = timestamp
+        
+        
         data = {"timestamp":timestamp, 'coords': {'longitude': longitude, "latitude": latitude}}
         results.append(data)
         i +=1
-        print (i)
+        print (f"Fetching coords {i*20}% complete")
         time.sleep(5)
-
-
+    
+    total_time = timestamp - first_timestamp
     names_dic = {"names": names}
     results.append(names_dic)
+    print ()
     print (results)
-    
+    print (f'Task complete, coords fetched in {total_time} seconds')    
     
     x = f"{timestamp}.json"
     with open (x,'a') as f:
         json.dump(results, f, indent=4)
+    
+    print (f"file written to {x}")
